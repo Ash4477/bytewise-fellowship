12ed2eff9a34c87550013d2931ad4c08fe46bc87
Adil <115704572+Ash4477@users.noreply.github.com>
2024-07-30 23:34:21 +0500

    finish proj

--- Month 2/week 2/JSX-Comps-Props/react-proj/src/App.js
+++ Month 2/week 2/JSX-Comps-Props/react-proj/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User name="Ash" version="Prime" universe={47}/>
     </div>
   );
 }
+
+const User = (props) => {
+  return <div>
+            <h1>Name: {props.name}</h1>
+            <h1>Variant: {props.version}</h1>
+            <h1>Universe: {props.universe}</h1>
+        </div>;
+};
 
 export default App;
