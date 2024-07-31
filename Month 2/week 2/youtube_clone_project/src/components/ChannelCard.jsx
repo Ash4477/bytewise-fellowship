bb9fd83a28b1140f6b5b2dfdf045a9bd9ce894ad
Adil <115704572+Ash4477@users.noreply.github.com>
2024-07-31 21:41:44 +0500

    work on channel Card portion

--- Month 2/week 2/youtube_clone_project/src/components/ChannelCard.jsx
+++ Month 2/week 2/youtube_clone_project/src/components/ChannelCard.jsx
@@ -4,8 +4,19 @@ import { Link } from "react-router-dom";
 
 import { demoProfilePicture } from "../utils/constants";
 
+// TODO: ChannelCard not showing + some videos not from specified channel
+
 const ChannelCard = ({ channelDetail }) => (
-  <Box sx={{ boxShadow: "none", borderRadius: "20px" }}>
+  <Box sx={{ 
+      boxShadow: "none",
+      borderRadius: "20px",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      width: { xs: "356px", md: "320px" },
+      height: "326px",
+      margin: "auto"
+    }}>
     <Link to={`/channel/${channelDetail?.id?.channelId}`}>
       <CardContent sx={{ display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "center", color: "#fff" }}>
         <CardMedia
