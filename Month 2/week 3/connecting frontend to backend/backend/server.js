9d9fccab99ab50a389c0c9cc588d3564d6d3843e
Adil <115704572+Ash4477@users.noreply.github.com>
2024-08-17 03:21:59 +0500

    done with backend part

---
+++ Month 2/week 3/connecting frontend to backend/backend/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+
+const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("Server is ready");
+// });
+
+// get a list of 4 jokes
+app.get("/jokes", (req, res) => {
+    const jokes = [
+        {
+            id: 1,
+            title: "Joke 1",
+            content: "This is a joke"
+        },
+        {
+            id: 2,
+            title: "Joke 2",
+            content: "This is also a joke"
+        },
+        {
+            id: 3,
+            title: "Joke 3",
+            content: "This is a joke too"
+        },
+        {
+            id: 4,
+            title: "Joke 4",
+            content: "This is another joke"
+        }
+    ];
+    res.send(jokes);
+});
+
+
+const port = process.env.PORT | 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+});
