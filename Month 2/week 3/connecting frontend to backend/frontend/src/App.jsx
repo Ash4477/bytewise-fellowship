486794fde3b6a8cf19ca5f3b42bfa44c01330b66
Adil <115704572+Ash4477@users.noreply.github.com>
2024-08-17 04:09:19 +0500

    finish frontend

---
+++ Month 2/week 3/connecting frontend to backend/frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [jokes, setJokes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/jokes')
+    .then((response) => setJokes(response.data))
+    .catch((err) => console.log(err));
+  });
+
+  return (
+    <>
+      <h1>This is Ash</h1>
+      <p>Here are some jokes: {(jokes.length)}</p>
+      {
+        jokes.map((joke) => (
+          <div key={joke.id}>
+            <h3>{joke.title}</h3>
+            <p>{joke.content}</p>
+          </div>
+        ))
+      }
+    </>
+  )
+}
+
+export default App
