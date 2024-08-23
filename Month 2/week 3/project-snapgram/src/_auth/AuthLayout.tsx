b35a0fbb22d11388eb192e0d9895d4be16ecb8cf
Adil <115704572+Ash4477@users.noreply.github.com>
2024-08-23 23:56:27 +0500

    finish auth

--- Month 2/week 3/project-snapgram/src/_auth/AuthLayout.tsx
+++ Month 2/week 3/project-snapgram/src/_auth/AuthLayout.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useUserContext } from '@/context/AuthContext';
+import { Outlet, Navigate } from 'react-router-dom';
 
 const AuthLayout = () => {
+  const isAuthenticated = useUserContext();
+
   return (
-    <div>AuthLayout</div>
+    <>
+      {isAuthenticated ? (<Navigate to='/' />) : (
+        <>
+          <section className='flex flex-1 justify-center items-center flex-col py-10'>
+            <Outlet />
+          </section>
+
+          <img
+            src='/assets/images/side-img.svg'
+            alt='logo'
+            className='hidden xl:block h-screen w-1/2 object-cover bg-no-repeat'
+          />
+        </>
+      )}
+    </>
   )
 }
 
---
+++ Month 2/week 3/project-snapgram/src/vite-env.d.ts
@@ -0,0 +1,1 @@
+/// <referece types="vite/client" />
