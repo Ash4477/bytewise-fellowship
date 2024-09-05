21fface159fd36630c3998e8747b6dd31e8d8ec0
Adil <115704572+Ash4477@users.noreply.github.com>
2024-09-05 20:52:26 +0500

    project setup

---
+++ Month 2/week 4/project-iphone-website/src/App.jsx
@@ -0,0 +1,15 @@
+import { Navbar, Hero, Highlights } from './components';
+import './App.css'
+
+const App = () => {
+
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights /> 
+    </main>
+  )
+}
+
+export default App
