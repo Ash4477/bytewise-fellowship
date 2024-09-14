712483b71b47c152f14c47da624c78cb1e31424b
Adil <115704572+Ash4477@users.noreply.github.com>
2024-09-14 11:17:46 +0500

    fix heading on top artists page

--- Month 2/week 4/project-spotify-clone/src/pages/TopArtists.jsx
+++ Month 2/week 4/project-spotify-clone/src/pages/TopArtists.jsx
@@ -21,7 +21,7 @@ const TopArtists = () => {
 
     return (
         <div className="flex flex-col">
-        <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Discover Top Charts</h2>
+        <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Discover Top Artists</h2>
 
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
             {artistData?.artists.map((artist, i) => (
