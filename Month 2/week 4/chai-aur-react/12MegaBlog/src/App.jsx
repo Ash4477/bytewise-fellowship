7b7ad9c5ce6b3be37fa34e735ecd003d14c92183
Adil <115704572+Ash4477@users.noreply.github.com>
2024-09-15 08:57:57 +0500

    setup redux + add tailwind + add header,footer

--- Month 2/week 4/chai-aur-react/12MegaBlog/src/App.jsx
+++ Month 2/week 4/chai-aur-react/12MegaBlog/src/App.jsx
@@ -1,12 +1,39 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login, logout} from './store/authSlice';
+import {Header, Footer} from './components';
+import { Outlet } from 'react-router-dom';
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>a blog app with appwrite</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .catch(() => console.log('Something went wrong'))
+    .finally(() => setLoading(false));
+  }, [])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
